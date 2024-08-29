7b4e065a3f8aae93b675b108509ff38135f1ab4e
sourav <souravchongrey@gmail.com>
2024-08-29 19:28:22 +0530

    add myclass

---
+++ app/src/main/java/com/example/kotlin_basic_2024/Myclass.kt
@@ -0,0 +1,11 @@
+package com.example.kotlin_basic_2024
+
+public class Myclass  {
+
+    companion object {
+        @JvmStatic
+        fun main (args:Array<String>){
+            println("Hello world!")
+        }
+    }
+}

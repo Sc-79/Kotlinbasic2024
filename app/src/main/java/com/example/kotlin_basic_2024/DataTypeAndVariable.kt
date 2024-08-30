810502a7a81d903db48bef5efbcfd6c61dad1269
sourav <souravchongrey@gmail.com>
2024-08-30 10:20:49 +0530

    Add variable

---
+++ app/src/main/java/com/example/kotlin_basic_2024/DataTypeAndVariable.kt
@@ -0,0 +1,29 @@
+package com.example.kotlin_basic_2024
+
+class DataTypeAndVariable {
+
+
+    companion object{
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello world!")
+        }
+    }
+}
+
+//    @JvmStatic
+//    fun main(array: Array<String>){
+////        int
+//        string
+//        Boolean
+//        Float
+//        Double
+//        Long
+//                Char
+//        Byte
+
+//
+//        println("hello")
+//    }
+
+//}

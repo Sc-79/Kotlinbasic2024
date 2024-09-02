8a944923523098709861abceaaf8e4cdd8d50d87
sourav <souravchongrey@gmail.com>
2024-09-02 07:51:31 +0530

    simple example of while loop

---
+++ app/src/main/java/com/example/kotlin_basic_2024/WhileAndDowhile.kt
@@ -0,0 +1,23 @@
+package com.example.kotlin_basic_2024
+
+class WhileAndDowhile {
+
+     companion object{
+         @JvmStatic
+         fun main (array: Array<String>){
+             println("print my do while")
+
+//                 ..........................
+//           while loop simple example
+             var n:Int = 0
+             while (n<10){
+                 println("print my number ut to 9 ${n}")
+                 n++;
+             }
+//             .....................
+
+
+
+         }
+     }
+}

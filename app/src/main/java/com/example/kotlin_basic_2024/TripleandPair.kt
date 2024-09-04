7aaba65d57c74feafdc716b7078c4400b50a36ac
sourav <souravchongrey@gmail.com>
2024-09-04 19:05:56 +0530

    basic example of paire

--- app/src/main/java/com/example/kotlin_basic_2024/TripleandPair.kt
+++ app/src/main/java/com/example/kotlin_basic_2024/TripleandPair.kt
@@ -9,6 +9,21 @@ class TripleandPair {
             var (a,b)= Pair("A", 1)
             println("$a, $b")
 
+//            2nd example of pair
+
+            var (x,y)=Pair("sourav", 32)
+            println("$x,$y")
+//      3rd example of pair
+
+            var name = Pair("sourav",54);
+            println("${name.first}, ${name.second} ")
+//            nested pair
+
+            var nested = Pair("Sourav" ,Pair("akash", Pair("nill", 1)))
+            println(nested.second.first)
+//            akash
+
+
         }
     }
 }

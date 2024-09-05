913c13c4fe79951460d4f2cf5c2812bf7b3d2b74
sourav <souravchongrey@gmail.com>
2024-09-05 16:23:31 +0530

    how to create basic class

---
+++ app/src/main/java/com/example/kotlin_basic_2024/CreateClass.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin_basic_2024
+
+
+fun main(){
+
+    val kb = CreateClass()
+    println("Acess class variable  kotlin   " + kb.SomeInteger)
+
+    val add = CreateClass().add(5,20)
+    println("my add number is " + add)
+
+
+}
+
+
+class CreateClass {
+    var SomeInteger = 20
+
+fun add(a:Int, b:Int): Int{
+    return a + b
+}
+
+}
+

2218b9e83aa009a70b75d1e00972ad15ec87d9da
sourav <souravchongrey@gmail.com>
2024-09-12 22:33:10 +0530

    how to create basic class and access inside variable

---
+++ app/src/main/java/com/example/kotlin_basic_2024/ClassConstructor.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_basic_2024
+
+
+fun main(){
+    ClassConstructor()
+}
+
+
+class ClassConstructor {
+
+    init {
+        println("show value " )
+    }
+
+}
---
+++ app/src/main/java/com/example/kotlin_basic_2024/ClassSample.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin_basic_2024
+
+
+fun main(){
+
+val kb = ClassSample()
+    println("The Integer is " + kb.someInt);
+
+}
+
+class ClassSample {
+  var someInt = 10;
+
+
+}

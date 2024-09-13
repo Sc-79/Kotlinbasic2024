68e9cfd90b73dd529d2f41cd1d7df7449618f0ac
sourav <souravchongrey@gmail.com>
2024-09-13 08:09:45 +0530

    add some class inside func, variable, intger

--- app/src/main/java/com/example/kotlin_basic_2024/ClassSample.kt
+++ app/src/main/java/com/example/kotlin_basic_2024/ClassSample.kt
@@ -5,11 +5,16 @@ fun main(){
 
 val kb = ClassSample()
     println("The Integer is " + kb.someInt);
+    println("my function return " +kb.Add(4,6) )
 
 }
 
 class ClassSample {
   var someInt = 10;
+
+    fun Add(a:Int, b:Int):Int{
+        return  a + b
+    }
 
 
 }

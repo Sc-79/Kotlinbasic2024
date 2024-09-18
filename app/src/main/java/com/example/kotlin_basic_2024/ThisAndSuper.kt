42bcf6b43b182664a7fa0cabb6aa311c67fe9e65
sourav <souravchongrey@gmail.com>
2024-09-18 09:30:21 +0530

    basic example of this keyWord

--- app/src/main/java/com/example/kotlin_basic_2024/ThisAndSuper.kt
+++ app/src/main/java/com/example/kotlin_basic_2024/ThisAndSuper.kt
@@ -2,9 +2,24 @@ package com.example.kotlin_basic_2024
 
 
 fun main (){
+var Audi = Audi()
+    Audi.interior(1234)
+}
+open class Car {
 
 }
 
-class ThisAndSuper {
+
+class Audi : Car (){
+    var chasisNo =10
+
+    fun interior (chasisNo:Int){
+//        ans 1234
+        println("ChasisNo is " +chasisNo )
+
+//        now using This ans:- 10
+        println("ChasisNo is " + this.chasisNo)
+
+    }
 
 }
